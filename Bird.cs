6587cc8b7bff1d2d2dac98713128a48f365324e2
Jeremiah Brannon <jeremiah.brannon@gmail.com>
2024-08-30 13:37:56 -0600

    Testing bird moving

--- Bird.cs
+++ Bird.cs
@@ -7,10 +7,12 @@ public partial class Bird : CharacterBody2D
 	public AnimatedSprite2D bird_sprite;
 	
 	public const float JumpVelocity = -400.0f;
+	public const float Speed = 250f;
 	
 	public override void _Ready()
 	{
 		bird_sprite.Play("Fly");
+		Velocity = new Vector2(Speed, 0);
 	}
 
 	public override void _PhysicsProcess(double delta)
--- Main.cs
+++ Main.cs
@@ -1,18 +1,34 @@
 using Godot;
-using System;
+using System.Collections.Generic;
 
 public partial class Main : Node
 {
-	
+	private int _speed = 400;
+	private bool _flying = true;
+	private PackedScene _topBottomPipeScene;
+	private Node2D _lastPipe;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		_topBottomPipeScene = ResourceLoader.Load<PackedScene>("./TopBottomPipe.tscn");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (ShouldCreateNewPipe()) {
+			var curPipe = _topBottomPipeScene.Instantiate();
+			AddChild(curPipe);
+
+			curPipe._Set("linear_velocity", _speed);
+		}
+	}
+
+	private bool ShouldCreateNewPipe() {
+		if(_flying )
+			return true;
 		
+		return false;
 	}
 }
