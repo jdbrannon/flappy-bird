d2e81e8f69f9c877a446c55357e483ddd8cbfeb1
Jeremiah Brannon <jdbrannon@wustl.edu>
2024-08-29 23:04:27 -0600

    Bird flies

--- Bird.cs
+++ Bird.cs
@@ -3,8 +3,15 @@ using System;
 
 public partial class Bird : CharacterBody2D
 {
-	public const float Speed = 300.0f;
+	[Export]
+	public AnimatedSprite2D bird_sprite;
+	
 	public const float JumpVelocity = -400.0f;
+	
+	public override void _Ready()
+	{
+		bird_sprite.Play("Fly");
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -17,21 +24,9 @@ public partial class Bird : CharacterBody2D
 		}
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (Input.IsActionJustPressed("flap"))
 		{
 			velocity.Y = JumpVelocity;
-		}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 		}
 
 		Velocity = velocity;
